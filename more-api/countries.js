a10f6eb88ab011321570dac97e260b8171773d18
maruf-hussain <engmarufhussain@gmail.com>
2023-07-31 22:00:02 +0600

    alfjalkjfalkfja

---
+++ more-api/countries.js
@@ -0,0 +1,48 @@
+const loadCountries = () => {
+    fetch('https://restcountries.com/v3.1/all')
+    .then(res => res.json())
+    .then(data => displayCoutries(data))
+}
+
+const displayCoutries = countries => {
+   const countriesContainer = document.getElementById('all-countries');
+   countries.forEach(country => {
+    const countriesDiv = document.createElement('div');
+  
+    countriesDiv.classList.add('country')
+   
+
+        countriesDiv.innerHTML = `
+        <img src="${country.flags.png}" alt="">
+        <h1>Name: ${country.name.common}</h1>
+        <h3>Capital: ${country.capital ? country.capital[0] : 'No Capital'}</h3>
+       <button onClick="loadCountryDetails('$(country.cca3)')">Details</button>
+    
+        `;
+        countriesContainer.appendChild(countriesDiv);
+   })
+   
+};
+
+const loadCountryDetails = (code) =>{
+const url = `https://restcountries.com/v3.1/alpha/${code}`
+fetch(url)
+.then(res => res.json())
+.then(data => displayCountryDetails(data[0]))
+
+};
+
+const displayCountryDetails = country =>{
+    const countryDetails = document.getElementById('country-detail');
+   countryDetails.innerHTML =`
+   <h1>${country.name.common}</h1>
+   <img  src="${country.flags.png}" alt="">
+   `;
+ 
+}
+
+
+
+
+loadCountries()
+
